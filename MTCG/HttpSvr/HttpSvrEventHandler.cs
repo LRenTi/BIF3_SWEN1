84f575b24be8c447f9a6635d0ace875542bfb50e
Lorenz <lorenzrenti@outlook.com>
2024-11-25 19:53:52 +0100

    Initial commit

---
+++ MTCG/HttpSvr/HttpSvrEventHandler.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace MTCG
+{
+    /// <summary>This delegate is used for <see cref="HttpSvr"/> events.</summary>
+    /// <param name="sender">Sending object.</param>
+    /// <param name="e">Event arguments.</param>
+    public delegate void HttpSvrEventHandler(object sender, HttpSvrEventArgs e);
+}
