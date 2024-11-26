a763a009abda55de4a15ab5b6adf852983569069
Lorenz <lorenzrenti@outlook.com>
2024-11-26 17:19:19 +0100

    Beautify CurlScript

--- MonsterTradingCards.sh
+++ MonsterTradingCards.sh
@@ -19,49 +19,61 @@ done
 if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
 
 # --------------------------------------------------
-echo "1) Create Users (Registration)"
+echo .
+echo "### 1) Create Users (Registration) ###"
 # Create Users
 curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"kienboec\", \"password\":\"daniel\", \"fullname\":\"Clemens Kienboc\", \"email\":\"Clemens.Kienboc@gmail.com\"}"
-echo "Should return HTTP 200"
+echo .
+echo "-> Should return HTTP 200"
 echo .
 curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"altenhof\", \"password\":\"markus\", \"fullname\":\"Markus Altenhof\", \"email\":\"markus.altenhof@gmail.com\"}"
-echo "Should return HTTP 200"
+echo .
+echo "-> Should return HTTP 200"
 echo .
 curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"admin\", \"password\":\"istrator\", \"fullname\":\"Admin\", \"email\":\"admin@mtcg.com\"}"
-echo "Should return HTTP 200"
+echo .
+echo "-> Should return HTTP 200"
 echo .
 
 if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
 
-echo "should fail:"
+echo .
+echo "### Should fail: ###"
 curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"kienboec\", \"password\":\"daniel\"}"
-echo "Should return HTTP 4xx - User already exists"
+echo .
+echo "-> Should return HTTP 4xx - User already exists"
 echo .
 curl -i -X POST http://localhost:12000/users --header "Content-Type: application/json" -d "{\"username\":\"kienboec\", \"password\":\"different\"}"
-echo "Should return HTTP 4xx - User already exists"
+echo .
+echo "-> Should return HTTP 4xx - User already exists"
 echo . 
 echo .
 
 if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
 
 # --------------------------------------------------
-echo "2) Login Users"
+echo .
+echo "### 2) Login Users ###"
+echo .
 curl -i -X POST http://localhost:12000/sessions --header "Content-Type: application/json" -d "{\"username\":\"kienboec\", \"password\":\"daniel\"}"
-echo "should return HTTP 200 with generated token for the user"
+echo .
+echo "-> should return HTTP 200 with generated token for the user"
 echo .
 curl -i -X POST http://localhost:12000/sessions --header "Content-Type: application/json" -d "{\"username\":\"altenhof\", \"password\":\"markus\"}"
-echo "should return HTTP 200 with generated token for the user"
+echo .
+echo "-> should return HTTP 200 with generated token for the user"
 echo .
 curl -i -X POST http://localhost:12000/sessions --header "Content-Type: application/json" -d "{\"username\":\"admin\",    \"password\":\"istrator\"}"
-echo "should return HTTP 200 with generated token for the user"
+echo "-> should return HTTP 200 with generated token for the user"
 echo .
 
 if [ $pauseFlag -eq 1 ]; then read -p "Press enter to continue..."; fi
 
-echo "should fail:"
+echo .
+echo "### Should fail: ###"
+echo .
 curl -i -X POST http://localhost:12000/sessions --header "Content-Type: application/json" -d "{\"username\":\"kienboec\", \"password\":\"different\"}"
-echo "Should return HTTP 4xx - Login failed"
 echo .
+echo "-> Should return HTTP 4xx - Login failed"
 echo .
-
-echo "END"
+echo "### END ###"
